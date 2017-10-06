e20a4ba64556afa7c80019d838cca1d6e9622d81
Beringer <JBeringer@wescodist.com>
2017-10-06 11:43:45 -0400

    10/06/2017 | ContactController Rebuild | Changed the controller to work off the EmailAudit table

--- coreApplication/src/com/coreApplication/java/SQL/ContactController.java
+++ coreApplication/src/com/coreApplication/java/SQL/ContactController.java
@@ -7,16 +7,14 @@ import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
-
 import java.sql.ResultSet;
 
 public class ContactController {
 	
-	static String connStr = GetSQLConnection.getConnectionString();
-	
-	public static ResultSet checkCount (String email) {
+	private static String connStr = GetSQLConnection.getConnectionString();
+		
+	private static ResultSet checkCount (String query) {
 		ResultSet rs;
-		String query = "SELECT intContactCountLastMonth, intContactCountThisWeek, intContactCountToday FROM ContactController WHERE strEmailAddress = '" + email + "'";
 		try {
 			DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
 			Connection conn = DriverManager.getConnection(connStr);
@@ -31,97 +29,63 @@ public class ContactController {
 		return rs;
 	}
 	
-	public static Boolean emailViolationCheck (int lastMonth, int thisWeek, int today) {
-		Boolean allow;
-		if (lastMonth >= 30 || thisWeek >= 15 || today >= 5) {
-			allow = false;
-		} else {
-			allow = true;
-		}
-		return allow;
-	}
-	
-	public static Boolean contactPermission (String email) {
-		Boolean chk = false;
-		ResultSet r = checkCount(email);
+	private static Integer lastDay(String email) {
+		int count = 0;
+		String query = "SELECT COUNT(*) AS COUNT FROM EmailAudit WHERE strEmailAddress = '" + email + "' AND dtLastSend > DATEADD(DAY,-1,GETDATE())";
+		ResultSet rs = checkCount(query);
 		try {
-			while (r.next()) {
-					int month = r.getInt("intContactCountLastMonth");
-					int week = r.getInt("intContactCountThisWeek");
-					int today = r.getInt("intContactCountToday");
-					chk = emailViolationCheck(month,week,today);
+			while (rs.next()) {
+				count = rs.getInt("COUNT");
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
+			DefaultLogger.logMsg("ContactController.lastDay() Error! " + e.getMessage(), "ERR");
+		}
+		return count;
+	}
+	
+	private static Integer lastWeek(String email) {
+		int count = 0;
+		String query = "SELECT COUNT(*) AS COUNT FROM EmailAudit WHERE strEmailAddress = '" + email + "' AND dtLastSend > DATEADD(DAY,-7,GETDATE())";
+		ResultSet rs = checkCount(query);
+		try {
+			while (rs.next()) {
+				count = rs.getInt("COUNT");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			DefaultLogger.logMsg("ContactController.lastDay() Error! " + e.getMessage(), "ERR");
+		}
+		return count;
+	}
+	
+	private static Integer lastMnth(String email) {
+		int count = 0;
+		String query = "SELECT COUNT(*) AS COUNT FROM EmailAudit WHERE strEmailAddress = '" + email + "' AND dtLastSend > DATEADD(DAY,-30,GETDATE())";
+		ResultSet rs = checkCount(query);
+		try {
+			while (rs.next()) {
+				count = rs.getInt("COUNT");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			DefaultLogger.logMsg("ContactController.lastMnth() Error! " + e.getMessage(), "ERR");
+		}
+		return count;
+	}
+	
+	public static Boolean permissionCheck(String email) {
+		Boolean chk = false;
+		int month = lastMnth(email);
+		int week = lastWeek(email);
+		int day = lastDay(email);
+		if (month > 30 || week > 15 || day > 5) {
+			chk = false;
+			DefaultLogger.logMsg("User contact frequency violation!", "WAR");
+		} else {
+			chk = true;
+			DefaultLogger.logMsg("User permission pass...", "INF");
 		}
 		return chk;
-	}
-	
-	public static void contactUpdate (String email) {
-		String query = "UPDATE ContactController SET intContactCountLastMonth = intContactCountLastMonth + 1, intContactCountThisWeek = intContactCountThisWeek + 1, intContactCountToday = intContactCountToday + 1 WHERE strEmailAddress = '" + email + "'";
-		try {
-			DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
-			Connection conn = DriverManager.getConnection(connStr);
-			Statement sta = conn.createStatement();
-			sta.executeUpdate(query);
-		} catch (SQLException e) {
-			e.printStackTrace();
-			DefaultLogger.logMsg("Error at contactUpdate [ContactController.java]: " + e.getMessage(), "ERR");
-		}
-	}
-	
-	public static void purgeMonth () {
-		String query = "UPDATE ContactController SET intContactCountLastMonth = 0";
-		try {
-			DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
-			Connection conn = DriverManager.getConnection(connStr);
-			Statement sta = conn.createStatement();
-			sta.executeUpdate(query);
-		} catch (SQLException e) {
-			e.printStackTrace();
-			DefaultLogger.logMsg("Error at purgeDaily [ContactController.java]: " + e.getMessage(), "ERR");
-		}
-	}
-	
-	public static void purgeWeek () {
-		String query = "UPDATE ContactController SET intContactCountThisWeek = 0";
-		try {
-			DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
-			Connection conn = DriverManager.getConnection(connStr);
-			Statement sta = conn.createStatement();
-			sta.executeUpdate(query);
-		} catch (SQLException e) {
-			e.printStackTrace();
-			DefaultLogger.logMsg("Error at purgeDaily [ContactController.java]: " + e.getMessage(), "ERR");
-		}
-	}
-	
-	public static void purgeDaily () {
-		String query = "UPDATE ContactController SET intContactCountToday = 0";
-		try {
-			DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
-			Connection conn = DriverManager.getConnection(connStr);
-			Statement sta = conn.createStatement();
-			sta.executeUpdate(query);
-		} catch (SQLException e) {
-			e.printStackTrace();
-			DefaultLogger.logMsg("Error at purgeDaily [ContactController.java]: " + e.getMessage(), "ERR");
-		}
-	}
-
-	public static void main(String[] args) {
-		String email = "jberinger@wesco.com";
-		System.out.println("Checking " + email);
-		ResultSet result = checkCount(email);
-		
-		try {
-			while (result.next()) {
-					System.out.println(result.getInt("intContactCountLastMonth"));
-					System.out.println(result.getInt("intContactCountThisWeek"));
-					System.out.println(result.getInt("intContactCountToday"));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
 	}
 }
